1023c45906b173e298af9a7e7580a044dc09c959
Andrew Kane <andrew@ankane.org>
2021-03-13 16:26:22 -0800

    Improved code [skip ci]

--- ext/datasketches/kll_wrapper.cpp
+++ ext/datasketches/kll_wrapper.cpp
@@ -13,11 +13,11 @@ namespace Rice::detail
   {
     static std::vector<T> convert(VALUE x)
     {
-      auto a = Rice::Array(x);
+      auto len = RARRAY_LEN(x);
       std::vector<T> vec;
-      vec.reserve(a.size());
-      for (long i = 0; i < a.size(); i++) {
-        vec.push_back(From_Ruby<T>::convert(a[i].value()));
+      vec.reserve(len);
+      for (long i = 0; i < len; i++) {
+        vec.push_back(From_Ruby<T>::convert(rb_ary_entry(x, i)));
       }
       return vec;
     }
@@ -30,7 +30,7 @@ namespace Rice::detail
     {
       auto a = rb_ary_new2(x.size());
       for (const auto& v : x) {
-        rb_ary_push(a, Rice::detail::To_Ruby<T>::convert(v, takeOwnership));
+        rb_ary_push(a, To_Ruby<T>::convert(v, takeOwnership));
       }
       return a;
     }
